0f3498af2e7f8857e3d731e617cfd89007a9dc5e
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-08 14:27:54 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,17 +7,17 @@ def using_Sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse!
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
